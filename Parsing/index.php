ea2fd09c17c09beeec329a9231f977e850f08472
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-09 19:45:53 +0300

    get all hrefs

--- Parsing/index.php
+++ Parsing/index.php
@@ -4,4 +4,13 @@ require_once "simple_html_dom.php";
 
 $url = "http://ananaska.com/vse-novosti/";
 
-$html = file_get_html($url);
+function getArticleLinksFromCatalog($url)
+{
+    $html = file_get_html($url);
+
+    foreach ($html->find('a.read-more-link') as $link_to_article) {
+        echo $link_to_article->href . PHP_EOL;
+    }
+}
+
+getArticleLinksFromCatalog($url);
