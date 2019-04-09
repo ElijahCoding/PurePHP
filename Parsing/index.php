bdb93495df87bb931d27f6a561ed7153d479b2cd
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-09 19:55:14 +0300

    recursion function to get all pages

--- Parsing/index.php
+++ Parsing/index.php
@@ -6,10 +6,17 @@ $url = "http://ananaska.com/vse-novosti/";
 
 function getArticleLinksFromCatalog($url)
 {
+    echo PHP_EOL . $url . PHP_EOL . PHP_EOL;
+    
     $html = file_get_html($url);
 
     foreach ($html->find('a.read-more-link') as $link_to_article) {
         echo $link_to_article->href . PHP_EOL;
+    }
+
+    // Recursion
+    if ($next_link = $html->find('a.next', 0)) {
+        getArticleLinksFromCatalog($next_link->href);
     }
 }
 
