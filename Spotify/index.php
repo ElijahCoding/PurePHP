d941532013c45d17671b3d99ef07a7a93776cbcf
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-15 16:14:47 +0300

    building index page

--- Spotify/index.php
+++ Spotify/index.php
@@ -11,10 +11,31 @@ if (isset($_SESSION['userLoggedIn'])) {
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
-	Hello!
+    <div id="nowPlayingBarContainer">
+		<div id="nowPlayingBar">
+			<div id="nowPlayingLeft">
+                <div class="content">
+                    <span class="albumLink">
+						<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+					</span>
+
+                    <div class="trackInfo">
+                        <span class="trackName">
+							<span>Happy Birthday</span>
+						</span>
+
+						<span class="artistName">
+							<span>Reece Kenney</span>
+						</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
