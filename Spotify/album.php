1b8d759e2781a25bddf9b720b0e1615b32a0784f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-16 11:15:13 +0300

    List songs

--- Spotify/album.php
+++ Spotify/album.php
@@ -31,7 +31,32 @@ $artist = $album->getArtist();
             $i = 1;
 
             foreach ($songIdArray as $songId) {
-                
+                $albumSong = new Song($con, $songId);
+                $albumArtist = $albumSong->getArtist();
+
+                echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play-white.png'>
+						<span class='trackNumber'>$i</span>
+					</div>
+
+
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+
+					<div class='trackOptions'>
+						<img class='optionsButton' src='assets/images/icons/more.png'>
+					</div>
+
+					<div class='trackDuration'>
+						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					</div>
+
+
+				</li>";
+                $i = $i + 1;
             }
         ?>
     </ul>
