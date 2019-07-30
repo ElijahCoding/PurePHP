a47876229ce3e2a2e59edd523e39ca16c8c864c2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-30 23:57:40 +0300

    config built

--- PHPeCommerce/config/init.php
+++ PHPeCommerce/config/init.php
@@ -9,3 +9,16 @@ define("LIBS", ROOT . '/vendor/ishop/core/libs');
 define("CACHE", ROOT . '/tmp/cache');
 define("CONF", ROOT . '/config');
 define("LAYOUT", 'default');
+
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+// http://ishop2.loc/public/index.php
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+// http://ishop2.loc
+$app_path = str_replace('/public/', '', $app_path);
+
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
