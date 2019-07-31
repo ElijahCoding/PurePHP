04f796160496bbe770af3e43e6a635f7d44d79b2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-31 14:27:42 +0300

    fetch categories

--- eCommerce/functions.php
+++ eCommerce/functions.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+* Распечатка массива
+**/
+function print_arr($array) {
+	echo "<pre>" . print_r($array, true) . "</pre>";
+}
+
+/**
+* Получение массива категорий
+**/
+function get_cat() {
+    global $connection;
+    $query = "SELECT * FROM categories";
+    $res = mysqli_query($connection, $query);
+
+    $arr_cat = array();
+
+    while ($row = mysqli_fetch_assoc($res)) {
+        $arr_cat[] = $row;
+    }
+
+    return $arr_cat;
+}
--- eCommerce/index.php
+++ eCommerce/index.php
@@ -1,5 +1,29 @@
 <?php
 
 include 'config.php';
+include 'functions.php';
+
+$categories = get_cat();
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Каталог</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <a href="/">Главная</a>
+        <div class="wrapper">
+            <div class="sidebar">
+                Sidebar
+            </div>
+            <div class="content">
+                <?php print_arr($categories); ?>
+            </div>
+        </div>
+    </body>
+</html>
